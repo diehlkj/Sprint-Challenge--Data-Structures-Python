3747a178740cd6f03037794b537fe514a8c6d8d9
Ken Diehl <diehlkj.99@gmail.com>
2020-09-25 21:41:56 -0400

    ring_buffer completed

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,25 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
-
-    def append(self, item):
-        pass
+        self.capacity = capacity
+        self.contents = []
+        self.clock = 0
+        
+    def __len__(self):
+        return len(self.contents)
+    
+    def append(self, item): 
+        if len(self.contents) == self.capacity:
+            del self.contents[self.clock]
+            self.contents.insert(self.clock, item)
+            
+            if self.clock == self.capacity - 1:
+                self.clock = 0
+            else:
+                self.clock += 1
+            
+        else:
+            self.contents.append(item)
 
     def get(self):
-        pass
+        contents = [item for item in self.contents if item is not None]
+        return contents
